8385f1a82a886638fc89d66e87467bc7d53ea515
alberto <albertosb124@gmail.com>
2021-01-20 00:12:45 -0600

    cambios

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const {
+    series,
+    src,
+    dest,
+    watch
+} = require('gulp');
+const sass = require('gulp-sass');
+
+//funcion que compila SASS
+
+function css() {
+    return src('src/scss/app.scss')
+        .pipe(sass())
+        .pipe(dest('./build/css'))
+}
+
+function minificarcss() {
+    return src('src/scss/app.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(dest('./build/css'))
+}
+
+function watchArchivos() {
+    watch('src/scss/**/*.scss',css); // * la carpeta actual ** = todos los archivos con esa extension
+}
+
+exports.css = css;
+exports.minificarcss = minificarcss;
+exports.watchArchivos=  watchArchivos;
