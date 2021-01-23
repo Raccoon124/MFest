05ac0a6769f40880166a4adc21288b44fb59f6c7
alberto <albertosb124@gmail.com>
2021-01-23 01:29:28 -0600

    23 enero cambios

--- gulpfile.js
+++ gulpfile.js
@@ -2,37 +2,89 @@ const {
     series,
     src,
     dest,
-    watch
+    watch,
+    parallel
 } = require('gulp');
 const sass = require('gulp-sass');
-const imagemin =require('gulp-imagemin');
+const imagemin = require('gulp-imagemin');
+const notify = require('gulp-notify');
+const webp = require('gulp-webp');
+const concat = require('gulp-concat')
+
+//utilidades CSS
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+
+//Utilidades JS
+const terser = require('gulp-terser-js');
+const rename = require('gulp-rename');
+
 
 //funcion que compila SASS
 
+const paths = {
+    imagenes: 'src/img/**/*',
+    scss: 'src/scss/**/*.scss',
+    js: 'src/js/**/*.js'
+}
+
 function css() {
-    return src('src/scss/app.scss')
+    return src(paths.scss)
+        .pipe(sourcemaps.init())
         .pipe(sass())
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('./build/css'))
 }
 
-function minificarcss() {
-    return src('src/scss/app.scss')
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }))
-        .pipe(dest('./build/css'))
+// function minificarcss() {
+//     return src(paths.scss)
+//         .pipe(sass({
+//             outputStyle: 'compressed'
+//         }))
+//         .pipe(dest('./build/css'))
+// }
+
+function javascript() {
+    return src(paths.js)
+        .pipe(sourcemaps.init())
+        .pipe(concat('bundle.js'))
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(dest('./build/js'))
 }
-function imagenes(){
-    return src('src/img/**/*')
-    .pipe(imagemin())
-    .pipe(dest('./build/img'))
+
+function imagenes() {
+    return src(paths.imagenes)
+        .pipe(imagemin())
+        .pipe(dest('./build/img'))
+        .pipe(notify({
+            message: 'Imagen Minificada'
+        }));
+}
+
+function versionWebp() {
+    return src(paths.imagenes)
+        .pipe(webp())
+        .pipe(dest('./build/img'))
+        .pipe(notify({
+            message: 'Version webp Lista'
+        }));
 }
 
 function watchArchivos() {
-    watch('src/scss/**/*.scss',css); // * la carpeta actual ** = todos los archivos con esa extension
+    watch(paths.scss, css); // * la carpeta actual ** = todos los archivos con esa extension
+    watch(paths.js, javascript);
 }
 
+
+
 exports.css = css;
-exports.minificarcss = minificarcss;
+// exports.minificarcss = minificarcss;
 exports.imagenes = imagenes;
-exports.watchArchivos=  watchArchivos;
+exports.watchArchivos = watchArchivos;
+
+exports.default = series(css, javascript, imagenes, versionWebp, watchArchivos);
